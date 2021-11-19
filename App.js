ac5d3fb12ac9b25723b8a1d1f230736be9eda3ec
A13DD12345 <88482824+a13dd12345@users.noreply.github.com>
2021-11-19 23:58:22 +0200

    fix: style import

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 
-import { globalStyles } from './styles/'
+import { globalStyles } from './styles'
 
 const styles = {
   ...globalStyles,
