86ec7b1514c538c5ad6c79f5acf74aecd4f16ee6
A13DD12345 <88482824+a13dd12345@users.noreply.github.com>
2021-11-20 00:32:05 +0200

    add styles App.js screen

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { Text, View, TextInput, Button } from 'react-native';
 
 
 import { globalStyles } from './styles'
+import appStyles from './appStyles'
 
 const styles = {
   ...globalStyles,
+  ...appStyles,
 }
 
 export default function App() {
   return (
-    <View style={styles.empty}>
+    <View style={styles.screen}>
       <Text>Courses Goals v. 09-00</Text>
       <TextInput />
       <Button title="add goal"/>
